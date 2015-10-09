8f0e5135c094fcd75dad69aa80c79e554232ca31
Aneesh Roy <roy.aneesh@gmail.com>
2015-10-09 15:31:46 +0100

    Basic setup for Windows

---
+++ setup_win32.bat
@@ -0,0 +1,2 @@
+mklink %CD%\.vimrc %HOMEDRIVE%%HOMEPATH%_vimrc
+mklink %CD%\.vimperatorrc %HOMEDRIVE%%HOMEPATH%_vimperatorrc
---
+++ setup_win32_noadmin.bat
@@ -0,0 +1,2 @@
+echo source %CD%\.vimrc>%HOMEDRIVE%%HOMEPATH%_vimrc
+echo source %CD%\.vimperatorrc>%HOMEDRIVE%%HOMEPATH%_vimperatorrc
